aa7aaa9974bd5459ae14b6831a9d9069e5f4d4d2
santos177 <santos177@gmail.com>
2017-11-27 00:28:43 -0300

    improving the test files

--- omnicore-master/src/omnicore/test/mdex_custom.cpp
+++ omnicore-master/src/omnicore/test/mdex_custom.cpp
@@ -1,4 +1,5 @@
 #include "omnicore/test/utils_tx.h"
+#include "omnicore/tally.h"
 #include "omnicore/mdex.h"
 #include "omnicore/mdex.cpp"
 #include "omnicore/errors.h"
@@ -46,7 +47,8 @@ BOOST_FIXTURE_TEST_SUITE(omnicore_mdexobject, BasicTestingSetup)
 
 BOOST_AUTO_TEST_CASE(test1)
 {
-        CMPTransaction x;   // transaction object 1
+        CMPTally tally;  // the tally map object
+        CMPTransaction x;   // transaction object  (the seller)
         const uint256 t;
         x.SetNull();
         uint64_t n1 = 20000;
@@ -54,7 +56,7 @@ BOOST_AUTO_TEST_CASE(test1)
         unsigned int idx1= 1;
         //int64_t bt1 = 3;
         int b1 = 1;
-        const std::string s1 = "WorldHello";
+        const std::string s1 = "1dexX7zmPen1yBz2H9ZF62AK5TGGqGTZH";
         const std::string r1 = "";
         unsigned int size1 = 10;
         unsigned char c1;
@@ -63,14 +65,14 @@ BOOST_AUTO_TEST_CASE(test1)
         int encodingClassIn = 2;
         x.Set(s1, r1, n1, t,b1, idx1, d1, size1, encodingClassIn, txf1);
 
-        CMPTransaction tr;  // transaction object 2
+        CMPTransaction tr;  // transaction object 2 (the buyer)
         tr.SetNull();
         uint64_t n=10000;
         uint64_t txf = 2;
         unsigned int idx= 1;
       //  int64_t bt = 3;
         int b = 1;
-        const std::string s = "HelloWorld";
+        const std::string s = "1zAtHRASgdHvZDfHs6xJquMghga4eG7gy";
         const std::string r = "";
         unsigned int size = 10;
         unsigned char c;
@@ -79,36 +81,47 @@ BOOST_AUTO_TEST_CASE(test1)
 
         //tr.Set(t,b,idx,bt);
         tr.Set(s, r, n, t,b, idx, d, size, encodingClassIn, txf);
-        CMPMetaDEx object(tr);
-        CMPMetaDEx object2(x);
+        CMPMetaDEx object2(tr);  //buyer
+        CMPMetaDEx object(x);  //seller
         CMPMetaDEx *p,*q;
         q = &object2;
         p = &object; // using pointers!
-
+        uint32_t propertyId = 0;
 
         BOOST_CHECK_EQUAL(0, p->getProperty());  // testing some properties of the object
         BOOST_CHECK_EQUAL(0, p->getDesProperty());
-        BOOST_CHECK_EQUAL("HelloWorld", p->getAddr());
+        BOOST_CHECK_EQUAL("1dexX7zmPen1yBz2H9ZF62AK5TGGqGTZH", p->getAddr());
         BOOST_CHECK_EQUAL(0,p->getAmountForSale());
         BOOST_CHECK_EQUAL(0,p->getAmountDesired());
         BOOST_CHECK_EQUAL(0,p->getAmountRemaining());
 
-
-
-        BOOST_CHECK_EQUAL(0, q->getProperty());  // testing some properties of the object
+        BOOST_CHECK_EQUAL(0, q->getProperty());  // testing some properties of the object2
         BOOST_CHECK_EQUAL(0, q->getDesProperty());
-        BOOST_CHECK_EQUAL("WorldHello", q->getAddr());
+        BOOST_CHECK_EQUAL("1zAtHRASgdHvZDfHs6xJquMghga4eG7gy", q->getAddr());
         BOOST_CHECK_EQUAL(0,q->getAmountForSale());
         BOOST_CHECK_EQUAL(0,q->getAmountDesired());
         BOOST_CHECK_EQUAL(0,q->getAmountRemaining());
 
 
-      //  BOOST_CHECK(mastercore::MetaDEx_INSERT(q));
+        BOOST_CHECK(mastercore::MetaDEx_INSERT(object));
+
+        BOOST_CHECK(mastercore::update_tally_map(s1, propertyId, 20000, BALANCE));  // putting some money here
+        BOOST_CHECK(mastercore::update_tally_map(s1, propertyId, 20000, METADEX_RESERVE));  // putting some money here
+        BOOST_CHECK(mastercore::update_tally_map(s, propertyId, 10000, BALANCE));
+        BOOST_CHECK(mastercore::update_tally_map(s, propertyId, 10000, METADEX_RESERVE));
+        BOOST_CHECK_EQUAL(20000,getMPbalance(s1, propertyId, BALANCE)); //checking balance of sender
+        BOOST_CHECK_EQUAL(20000,getMPbalance(s1, propertyId, METADEX_RESERVE));
+        BOOST_CHECK_EQUAL(10000,getMPbalance(s, propertyId, BALANCE));
+        BOOST_CHECK_EQUAL(10000,getMPbalance(s, propertyId, METADEX_RESERVE));
 
 
-      //  BOOST_CHECK_EQUAL(TRADED,x_Trade(p));  // is true if the match happens
+        BOOST_CHECK_EQUAL(TRADED,x_Trade(q));  // insert the buyer, is true if the match with seller happens
 
 
+        BOOST_CHECK_EQUAL(20000,getMPbalance(s1, propertyId, BALANCE)); //checking balance of sender p*
+        BOOST_CHECK_EQUAL(20000,getMPbalance(s1, propertyId, METADEX_RESERVE));
+        BOOST_CHECK_EQUAL(0,getMPbalance(s, propertyId, BALANCE));  // checking balance of receiver q*
+        BOOST_CHECK_EQUAL(0,getMPbalance(s, propertyId, METADEX_RESERVE));
 
 
 
